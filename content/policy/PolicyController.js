78aa15b4c2efb750060abde034928a0eddb659c8
Warren Parad <wparad@gmail.com>
2017-05-25 12:14:42 +0200

    Add about Serify section.

--- content/policy/PolicyController.js
+++ content/policy/PolicyController.js
@@ -1,6 +1,7 @@
 angular.module(SERIFYAPP).config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/policy', { templateUrl: 'policy/policy.html', controller: 'policyController' });
 	$routeProvider.when('/terms', { templateUrl: 'policy/termsOfService.html', controller: 'policyController' });
+	$routeProvider.when('/about', { templateUrl: 'policy/about.html', controller: 'policyController' });
 }]);
 angular.module(SERIFYAPP).controller('policyController', ['$scope', 'pageService', function($scope, pageService) {
 	$scope.PrivacyButtonClick = function() {
